3ad47a6c6e42e15e4c479681c9998c0129fa059a
Marcos B. Tsuda <tsudaspontodev@gmail.com>
2026-05-12 22:42:27 -0300

    criação
    criação de sistema para mecânica

---
+++ mecanica.php
@@ -0,0 +1,49 @@
+<?php
+    session_start();
+    if (!isset($_SESSION['nome'])) {
+        header('Location: index.php?status=erro&msg=Acesso negado');
+        exit;
+    }
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="content-language" content="pt-br">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="imagens/logo_aba.png" type="image/png">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <title>Mecanica</title>
+        <style>
+            body {
+                background-color: #ddd7d7;
+            }
+            .header {
+                float: right;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <img src="imagens/banner.png" width="50%" heigth="50%">
+            <?php
+                echo "<div class='header'>";
+                if (isset($_SESSION['nome'])) {
+                    $nome = $_SESSION['nome'];
+                    echo "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-person-circle' viewBox='0 0 16 16'>
+                        <path d='M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0'/>
+                        <path fill-rule='evenodd' d='M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1'/>
+                        </svg>&nbsp;<b>".$nome."</b> | <a href='sair.php' style='color: black; text-decoration: none; font-weight: bold;'>SAIR</a>";
+                }
+                echo "</div>";
+            ?>
+        </div>
+        <br/>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <nav>
+            <?php
+                include 'menu.php';
+            ?>
+        </nav>
+    </body>
+</html>
---
+++ mecanica.sql
@@ -0,0 +1,175 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 13/05/2026 às 03:33
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `mecanica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `ordens`
+--
+
+CREATE TABLE `ordens` (
+  `id` int(11) NOT NULL,
+  `data_entrada` datetime NOT NULL,
+  `data_saida` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `orden_servicos`
+--
+
+CREATE TABLE `orden_servicos` (
+  `id` int(11) NOT NULL,
+  `id_orden` int(11) NOT NULL,
+  `nome_cliente` varchar(180) NOT NULL,
+  `cpf` varchar(20) NOT NULL,
+  `veiculo` varchar(150) NOT NULL,
+  `placa` varchar(7) NOT NULL,
+  `tipo_servico` varchar(150) NOT NULL,
+  `pecas` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pecas`
+--
+
+CREATE TABLE `pecas` (
+  `codigo` int(11) NOT NULL,
+  `nome` varchar(200) NOT NULL,
+  `marca` varchar(150) NOT NULL,
+  `modelo` varchar(255) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `data_entrada` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `servicos`
+--
+
+CREATE TABLE `servicos` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(200) NOT NULL,
+  `categoria` varchar(150) NOT NULL,
+  `prioridade` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(180) NOT NULL,
+  `cpf` varchar(20) NOT NULL,
+  `funcao` varchar(40) NOT NULL,
+  `genero` varchar(20) NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `senha` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `cpf`, `funcao`, `genero`, `login`, `senha`) VALUES
+(1, 'marcos', '12312312312', 'developer', 'Masculino', 'admin', 'admin');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `ordens`
+--
+ALTER TABLE `ordens`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `orden_servicos`
+--
+ALTER TABLE `orden_servicos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `pecas`
+--
+ALTER TABLE `pecas`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- Índices de tabela `servicos`
+--
+ALTER TABLE `servicos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `ordens`
+--
+ALTER TABLE `ordens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `orden_servicos`
+--
+ALTER TABLE `orden_servicos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pecas`
+--
+ALTER TABLE `pecas`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `servicos`
+--
+ALTER TABLE `servicos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
